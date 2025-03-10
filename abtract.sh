b9574ddf97dbb1b2b264afe7d2ceb913163e9f28
yukino1710193 <sasukevp1959@gmail.com>
2025-03-10 11:49:20 +0700

    version lab , update flexible username

--- abtract.sh
+++ abtract.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # truyền file
 FILE=${1}
 IFS=$'\n'
@@ -7,26 +6,14 @@ for line in $(cat ${FILE})
 do
     echo ${line}
     IFS=","
-    read STT IDENTIFY_FILE IP_HOST <<<${line}
+    read STT IDENTIFY_FILE IP_HOST USERNAME <<<${line}
     if [[ "${STT}" == "STT" ]]; then
-    continue
+        continue
     fi  
     # Truyen file
-    scp -i "$IDENTIFY_FILE" ~/Projects/vm_setting/set_host.sh ubuntu@$IP_HOST:
-    scp -i "$IDENTIFY_FILE" ~/.ssh/id_rsa.pub ubuntu@$IP_HOST:
+    scp -i "$IDENTIFY_FILE" ~/Projects/vm_setting/set_host.sh ${USERNAME}@${IP_HOST}:
+    scp -i "$IDENTIFY_FILE" ~/.ssh/id_rsa.pub ${USERNAME}@${IP_HOST}:
     # thuc thi
-    ssh -i "$IDENTIFY_FILE" ubuntu@$IP_HOST 'sudo cat *.pub >> ~/.ssh/authorized_keys'
-    ssh -i "$IDENTIFY_FILE" ubuntu@$IP_HOST 'sudo sh set_host.sh'
+    ssh -t -i "$IDENTIFY_FILE" ${USERNAME}@${IP_HOST} 'sudo cat *.pub >> ~/.ssh/authorized_keys'
+    ssh -t -i "$IDENTIFY_FILE" ${USERNAME}@${IP_HOST} 'sudo sh set_host.sh'
 done
-# while IFS=, read -r IDENTIFY_FILE IP_HOST; do
-#     # Truyen file
-#     scp -i "$IDENTIFY_FILE" ~/Projects/vm_setting/set_host.sh ubuntu@$IP_HOST:
-#     scp -i "$IDENTIFY_FILE" ~/.ssh/master-node.pub ubuntu@$IP_HOST:
-#     # thuc thi
-#     ssh -i "$IDENTIFY_FILE" ubuntu@$IP_HOST 'sudo cat *.pub >> ~/.ssh/authorized_keys'
-#     ssh -i "$IDENTIFY_FILE" ubuntu@$IP_HOST 'sudo sh set_host.sh'
-# done < ~/.ssh/hosts.txt
-# 1,~/.ssh/worker-node03.pem,10.0.1.103
-# ~/.ssh/master-node.pem,10.0.0.100
-# ~/.ssh/worker-node01.pem,10.0.24.101
-# ~/.ssh/worker-node02.pem,10.0.24.102
--- set_host.sh
+++ set_host.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 ##### file /etc/sudoers
-
-sudo grep -q "^ubuntu ALL=(ALL) NOPASSWD:ALL" /etc/sudoers || echo "ubuntu ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
-
-# thực hiện khối lệnh 1 sẽ kiểm tra trong file /etc/sudoers có dòng ubuntu ... kia chưa -> kết quả trả về 0 hoặc 1 tương ứng với chưa có và có
-# nếu chưa có thì thực hiện khối lệnh sau : bao gồm tải input sau phần echo , thêm input đó vào cuối file /etc/sudoers
+USERNAME=${SUDO_USER:-$(whoami)}
+sudo grep -q "^$USERNAME ALL=(ALL) NOPASSWD:ALL" /etc/sudoers || echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 
 ##### sửa file /etc/ssh/sshd_config nếu không tìm thấy văn bản gốc thì sẽ không sửa ( yên tâm khi file đó đã được config thì sẽ ko bị config lại
 # gây trùng lặp)
@@ -15,6 +12,3 @@ sudo sed -i 's/^#AuthorizedKeysFile\t.ssh\/authorized_keys .ssh\/authorized_keys
 #####
 # restart sshd sau khi sửa config
 sudo systemctl restart sshd
-##### ADD KEY SSH
-# cd ~
-# sudo cat *.pub >> .ssh/authorized_keys
